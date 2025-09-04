395e45ef9a9b049db744da2ca79f699d15e80d37
marineKP52 <gls.maryna@gmail.com>
2025-09-04 15:42:57 +0300

    Final task completed.

--- Program.cs
+++ Program.cs
@@ -1,2 +1,11 @@
 ﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("My name is Marina.");
+Console.WriteLine();
+
+for (int i = 1; i <= 100; i++)
+{
+    if (i % 3 == 0) Console.Write("Feez");
+    if (i % 5 == 0) Console.Write("Buzz");
+    if (i % 3 != 0 && i % 5 != 0) Console.Write(i);
+    Console.WriteLine();
+}
